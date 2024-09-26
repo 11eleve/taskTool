10cc659cbef7e968b0eae0fb84982197a36c4c42
xxl <1106013850@qq.com>
2024-09-26 15:57:19 +0800

    feat: view组件

---
+++ tasktool/src/view/main.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from './header.vue';
+import Content from './content.vue';
+</script>
+
+<template>
+  <div class="display">
+    <Header />
+    <Content />
+  </div>
+</template>
+
+<style>
+.display {
+  display: flex;
+  flex-direction: column;
+}
+</style>
