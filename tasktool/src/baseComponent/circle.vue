e3c9b8fb6d35637e8a1bc13fc12625dcb4e0b226
xxl <1106013850@qq.com>
2024-09-26 21:09:20 +0800

    feat: 实现动画效果

--- tasktool/src/baseComponent/circle.vue
+++ tasktool/src/baseComponent/circle.vue
@@ -11,6 +11,12 @@
   width: 30px;
   border-radius: 50%;
   background-color: #fff;
+  border: 12px solid #afb4db;
+  transition: border 0.1s ease; /* 平滑过渡效果 */
+}
+.circle-outer:hover {
   border: 10px solid #afb4db;
+  height: 30px; /* 高度增加 */
+  width: 30px; /* 宽度增加 */
 }
 </style>
