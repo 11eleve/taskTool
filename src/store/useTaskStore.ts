e2787979dba36cf4065678f64eabc245dc63acd1
xxl <1106013850@qq.com>
2024-10-07 17:08:02 +0800

    feat: 添加测试数据

--- src/store/useTaskStore.ts
+++ src/store/useTaskStore.ts
@@ -6,11 +6,27 @@ export const useTaskStore = defineStore('task', () => {
   let activeTask = ''
   let taskMap = new Map()
 
+  //测试数据
+  taskMap.set('test', [])
+  taskMap.get('test').push([
+    {title: "创建项目", description: "step1", finished: false},
+    {title: "加载项目", description: "step2", finished: false}, 
+    {title: "运行项目", description: "step3", finished: false},
+    {title: "结束项目", description: "step4", finished: false}
+  ])
+
   function add() {
     if (taskMap.has(activeTask)) {
       return
     }
     taskMap.set(activeTask, [])
+  }
+
+  function addTest(name: string) {
+    if (taskMap.has(name)) {
+      return
+    }
+    taskMap.set(name, [])
   }
 
   function remove(name: string) {
@@ -27,6 +43,13 @@ export const useTaskStore = defineStore('task', () => {
     return taskMap.get(activeTask)
   }
 
+  function getTest(name: string) {
+    if (!taskMap.has(name)) {
+      return
+    }
+    return taskMap.get(name)
+  }
+
   function set(name: string, task: taskType) {
     if (!taskMap.has(name)) {
       return
@@ -39,6 +62,8 @@ export const useTaskStore = defineStore('task', () => {
     add,
     remove,
     get,
-    set
+    set,
+    getTest,
+    addTest
   }
 })
