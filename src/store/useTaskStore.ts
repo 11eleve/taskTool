261a09eafb960f1022d0721c4200b9b0ba3babc8
xxl <1106013850@qq.com>
2024-10-07 15:57:20 +0800

    feat: 添加set功能

--- src/store/useTaskStore.ts
+++ src/store/useTaskStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
+import { taskType } from '../util/taskType'
 
-export const useTaskStore = defineStore('task',() => {
+export const useTaskStore = defineStore('task', () => {
   let taskMap = new Map()
 
   function add(name: string) {
@@ -12,15 +13,29 @@ export const useTaskStore = defineStore('task',() => {
 
   function remove(name: string) {
     if (!taskMap.has(name)) {
-      return 
+      return
     }
     taskMap.delete(name)
   }
 
   function get(name: string) {
     if (!taskMap.has(name)) {
-      return 
+      return
     }
     return taskMap.get(name)
+  }
+
+  function set(name: string, task: taskType) {
+    if (!taskMap.has(name)) {
+      return
+    }
+    taskMap.get(name).push(task)
+  }
+
+  return {
+    add,
+    remove,
+    get,
+    set
   }
 })
