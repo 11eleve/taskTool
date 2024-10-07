46bb3a17cb0bf2b49c1acc2d90670f07cd36f2d8
xxl <1106013850@qq.com>
2024-10-07 16:05:41 +0800

    feat: 步骤条添加获取

--- src/components/progress.vue
+++ src/components/progress.vue
@@ -1,34 +1,21 @@
 <template>
   <el-steps style="max-width: 600px" :active="2" align-center>
-    <el-step v-for="item in taskArr" :title="item.title" :description="item.description" />
+    <el-step v-for="item in taskArr" :title="item.title" :description="item.description" @click="finish(item)" />
   </el-steps>
-  <button @click="create">创建</button>
 </template>
 
-<script setup lang="ts"> 
+<script setup lang="ts">
+import { useTaskStore } from '../store/useTaskStore';
 import type { taskType } from '../util/taskType';
 
-//测试数据
-const one : taskType = {
-  title: 'hello',
-  description: 'ok',
-  finished: true,
+const {get} = useTaskStore()
+
+let taskArr: taskType[] = []
+
+taskArr = get()
+
+const finish = (item: taskType) => {
+  item.finished = true
+  console.log('完成了')
 }
-
-const taskArr: taskType[] = []
-
-const create = () => {
-  
-}
-
-const add = (taskTitle: string, taskDescription: string) => {
-
-  taskArr.push({
-    title: taskTitle,
-    description: taskDescription,
-    finished: false
-  })
-}
-
-taskArr.push(one)
 </script>
